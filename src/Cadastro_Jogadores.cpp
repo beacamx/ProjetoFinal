cd43c7b866a7e78ccb8dbe6245c53837e8cac4f8
bernardohaas <bernardocoelhosd@gmail.com>
2024-07-29 23:48:54 -0300

    Update Cadastro_Jogadores.cpp

--- src/Cadastro_Jogadores.cpp
+++ src/Cadastro_Jogadores.cpp
@@ -3,7 +3,20 @@
 cadastro::cadastro(){
   load(this);
 }
-void cadastro::cadastrar(jogador * player_1, jogador * player_2){
+void cadastro::cadastrar(jogador * player_1){
+  if (find(player_1->get_name()){
+    mensagem avisa que já exist, colhe resposta se deseja sobrecadastro;
+    if (!sobre){
+      return;
+    }
+  }
   jogadores.push_back(player_1);
-  jogadores.push_back(player_2);
+}
+jogador * cadastro::find(string input_name){
+    for (auto run : jogadores){
+      if (run->get_name() == input_name){
+        return run;
+      }
+    }
+    return NULL;
 }
