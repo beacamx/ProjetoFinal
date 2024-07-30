20ddf925ffc4e1639ce22f39f49137e365f64623
bernardohaas <bernardocoelhosd@gmail.com>
2024-07-29 22:43:01 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,32 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    int operador;
+    unsigned int operador;
 
     cout << "Escolha um jogo: " << endl << "1 - Lig4" << endl << "2 - Reversi" << endl;
     cin >> operador;
 
     unique_ptr<Interface_Jogo> jogo;
-
+    /* proposta de looping que admite nova tentativa ante erro de entrada do usuário a partir da linha 12
+    while(1){
+        cout << "Escolha um jogo: " << endl << "1 - Lig4" << endl << "2 - Reversi" << endl << "0 - Sair" << endl;
+        cin >> operador;
+        switch(operador){
+            case 1:
+                jogo = make_unique<Interface_Lig4>();
+                return jogo->iniciarJogo();
+                break;
+            case 2:
+                jogo = make_unique<Interface_Reversi()>;
+                return jogo->IniciarJogo;
+            case 0:
+                mensagem de saida
+                return 0;
+            default:
+                mensagem de erro de entrada;
+        }
+    }
+    */
     if (operador == 1) {
         jogo = make_unique<Interface_Lig4>();
     } else if (operador == 2) {
