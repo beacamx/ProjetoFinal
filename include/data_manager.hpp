69f020cf4ce1bd62be2f74a4e941cd10f15e5b04
bernardohaas <bernardocoelhosd@gmail.com>
2024-08-01 20:03:45 -0300

    Update data_manager.hpp

--- include/data_manager.hpp
+++ include/data_manager.hpp
@@ -1,7 +1,8 @@
-#include <Cadastro_Jogadores.hpp>
+#include "Cadastro_Jogadores.hpp"
 #ifndef DATA_MANAGER_HPP
 #define DATA_MANAGER_HPP
 namespace data_manager{
+std::string default_name("game_data.lig4&Reversi");
 void load(cadastro&);
 void save(cadastro&);
 }
