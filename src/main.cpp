c13109f86f3a9ee727edc3cfed69706efbea8de8
beacamx <beatrizcgp1@gmail.com>
2024-08-08 18:36:17 -0300

    teste

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,18 @@
 #include <memory>
 
 #include "Interface/Interface_Tela_Inicial.hpp"
+#include "Interface/Interface_Define_Dimensao.hpp"
 
 using namespace std;
 
 int main() {
-    unique_ptr<Interface_Tela_Inicial> menu = make_unique<Interface_Tela_Inicial>();
-    menu->Run_Menu();
+    //testar tudo
+    // unique_ptr<Interface_Tela_Inicial> menu = make_unique<Interface_Tela_Inicial>();
+    // menu->Run_Menu();
+
+    //testar Lig4 (só pra teste)
+    unique_ptr<Interface_Define_Dimensao> dimensao;
+
+    dimensao = make_unique<Interface_Define_Dimensao>("Lig4");
+    dimensao->Run_Menu();
 }
