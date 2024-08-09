7c46d961aba5cc97b01742e0d4db9e585bcd21b3
bernardohaas <bernardocoelhosd@gmail.com>
2024-08-09 00:05:15 -0300

    Update Cadastro_Jogadores.cpp
    Adaptando a implementação a <set>

--- src/Cadastro_Jogadores.cpp
+++ src/Cadastro_Jogadores.cpp
@@ -1,38 +1,56 @@
 #include "../include/Cadastro_Jogadores.hpp"
-cadastro::cadastro(){
+cadastro::cadastro()
+{
   load();
 }
-cadastro::~cadastro(){
+cadastro::~cadastro()
+{
   save();
 }
-jogador * cadastro::find(std::string _name){ //encontra jogador por nome
-  for (auto _aux : jogadores){
-    if(_aux->get_name() == _name){
+jogador *cadastro::find(std::string _name)
+{ // encontra jogador por nome
+  for (auto _aux : jogadores)
+  {
+    if (_aux->get_name() == _name)
+    {
       return _aux;
     }
   }
   return NULL;
 }
-void cadastro::cadastrar(jogador * target){ //insere *jogador no vetor<*jogador>
-  if (auto _aux = find(target->get_name())){
-      throw std::runtime_error("override_player");
+void cadastro::cadastrar(jogador *target)
+{ // insere *jogador no vetor<*jogador>
+  if (auto _aux = find(target->get_name()))
+  {
+    throw std::runtime_error("override_player");
   }
   jogadores.insert(target);
 }
-void cadastro::remover(jogador * target){ //remove *jogador no vetor<*jogador>
-  unsigned int counter = 0;
-  for (auto _aux : jogadores){
-    if (_aux == target){
-      jogadores.erase(jogadores.begin() + counter);
-      return;
-    } 
-    counter++;
+
+void cadastro::remover(jogador *target)
+{ // remove *jogador na lista. Caso não encontre, lança runtime exception
+  std::set<jogador*, player_compare>::iterator posi = jogadores.find(find(target->get_name()));
+  if (posi != jogadores.end())
+  {
+    jogadores.erase(posi);
   }
+  else
+  {
     throw std::runtime_error("player_not_found");
+  }
 }
-void cadastro::load(){//carrega do arquivo padrao
+std::vector<jogador*> cadastro::player_list(){
+  std::vector<jogador*> build;
+  for (auto _aux : jogadores){
+    build.push_back(_aux);
+  }
+  return build;
+}
+void cadastro::load()
+{ // carrega do arquivo padrao
   std::fstream archive(default_name, std::ios::in);
-  if (!archive){
+  if (!archive)
+  {
     archive.close();
     return;
   }
@@ -41,51 +59,47 @@ void cadastro::load(){//carrega do arquivo padrao
   archive.seekg(0, archive.beg);
   char test_controll[70] = {};
   archive.read(test_controll, std::string(string_controll).length());
-  if ( ! (std::string(test_controll) == std::string(string_controll)) ){
+  if (!(std::string(test_controll) == std::string(string_controll)))
+  {
     throw std::runtime_error("data_corruption");
   }
   unsigned int n_registers;
   archive.read((char *)&n_registers, sizeof(unsigned int));
-  while (n_registers){
+  while (n_registers)
+  {
     char _input_name[20] = {};
     unsigned int n_of_wins_lig4;
     unsigned int n_of_wins_Reversi;
     unsigned int n_of_loses_lig4;
     unsigned int n_of_loses_Reversi;
     archive.read(_input_name, 20);
-    archive.read((char*) &n_of_wins_lig4, sizeof(unsigned int));
-    archive.read((char*) &n_of_wins_Reversi, sizeof(unsigned int));
-    archive.read((char*) &n_of_loses_lig4, sizeof(unsigned int));
-    archive.read((char*) &n_of_loses_Reversi, sizeof(unsigned int));
+    archive.read((char *)&n_of_wins_lig4, sizeof(unsigned int));
+    archive.read((char *)&n_of_wins_Reversi, sizeof(unsigned int));
+    archive.read((char *)&n_of_loses_lig4, sizeof(unsigned int));
+    archive.read((char *)&n_of_loses_Reversi, sizeof(unsigned int));
     cadastrar(new jogador(_input_name, n_of_wins_lig4, n_of_wins_Reversi, n_of_loses_lig4, n_of_loses_Reversi));
     n_registers--;
   }
-  if (archive.tellg() != final_position){
+  if (archive.tellg() != final_position)
+  {
     /*arquivo corrompido*/
-    bool decision = decisao();
-    if (decision){
-      return;
-    } else {
-      throw std::runtime_error("data_corruption");
-    }
+    throw std::runtime_error("data_corruption");
   }
   archive.close();
 }
-void cadastro::save(){//salva no arquivo padrao
+void cadastro::save()
+{ // salva no arquivo padrao
   std::fstream archive(default_name, std::ios::out | std::ios::trunc | std::ios::binary);
-  if (!archive){
-    //arquivo não pode ser salvo, deseja sair?
-    bool decision = decisao();
-    if (decision){
-      return;
-    } else {
-      std::runtime_error("fail");//catch na main -> retorna ao looping de jogo
-    }
+  if (!archive)
+  {
+    // arquivo não pode ser salvo, deseja sair?
+    std::runtime_error("fail"); // catch na main -> retorna ao looping de jogo
   }
   archive.write(string_controll, std::string(string_controll).length());
   unsigned int n_players = jogadores.size();
-  archive.write((char*) &n_players, sizeof(unsigned int));
-  for (auto _aux : jogadores){
+  archive.write((char *)&n_players, sizeof(unsigned int));
+  for (auto _aux : jogadores)
+  {
     char p_name[20] = {};
     _aux->get_name().copy(p_name, _aux->get_name().size(), 0);
     unsigned int input_wins_lig4 = _aux->get_wins_lig4();
@@ -93,10 +107,10 @@ void cadastro::save(){//salva no arquivo padrao
     unsigned int input_loses_lig4 = _aux->get_loses_lig4();
     unsigned int input_loses_Reversi = _aux->get_loses_Reversi();
     archive.write(p_name, 20);
-    archive.write((char*) &input_wins_lig4, sizeof(unsigned int));
-    archive.write((char*) &input_wins_Reversi, sizeof(unsigned int));
-    archive.write((char*) &input_loses_lig4, sizeof(unsigned int));
-    archive.write((char*) &input_loses_Reversi, sizeof(unsigned int));
+    archive.write((char *)&input_wins_lig4, sizeof(unsigned int));
+    archive.write((char *)&input_wins_Reversi, sizeof(unsigned int));
+    archive.write((char *)&input_loses_lig4, sizeof(unsigned int));
+    archive.write((char *)&input_loses_Reversi, sizeof(unsigned int));
   }
   archive.close();
 }
