9b16e0956394fa28d750cc75a68440fd6c504fa7
beacamx <beatrizcgp1@gmail.com>
2024-08-12 07:37:55 -0300

    Adicionando protecoes no cadastro do jogador 1

--- src/Interface/Interface_Cadastro_Jogador1.cpp
+++ src/Interface/Interface_Cadastro_Jogador1.cpp
@@ -153,22 +153,36 @@ void Interface_Cadastro_Jogador1::Loop_Events() {
                 } else if (posicao == 2) {
                     seleção_ativa = true;
                     Troca_Definicao_Entrada_Jogador troca_Definicao_Entrada_Jogador;
-                    string nome_jogador = caixa_de_texto1.Obter_Texto_Entrada();
-                    string apelido_jogador = caixa_de_texto2.Obter_Texto_Entrada();
+                    string nome_jogador = caixa_de_texto2.Obter_Texto_Entrada();
+                    string apelido_jogador = caixa_de_texto1.Obter_Texto_Entrada();
                     try {
-                        if(registro_geral.find(nome_jogador) == NULL && apelido_jogador.length() > 0) { 
+                        if(registro_geral.find(apelido_jogador) == NULL && nome_jogador.length() > 0 && apelido_jogador.length() > 0) { 
                             registro_geral.cadastrar(new jogador(nome_jogador, apelido_jogador, 0, 0, 0, 0));
                             registro_geral.save();
                             janela->close();
                             troca_Definicao_Entrada_Jogador.Troca_Definicao_Jogador();
-                        } else if (registro_geral.find(nome_jogador) == NULL && apelido_jogador.length() == 0){
+                        } else if (registro_geral.find(apelido_jogador) == NULL && nome_jogador.length() == 0 && apelido_jogador.length() > 0){
                             cerr << "Aviso: Digite seu nome";
+                            cout << endl;
                             aviso.setString("Aviso: Digite seu nome");
+                            Define_Aviso();
+                            seleção_ativa = false;
+                        } else if(apelido_jogador.length() == 0 && nome_jogador.length() > 0) {
+                            cerr << "Aviso: Digite seu apelido";
+                            cout << endl;
+                            aviso.setString("Aviso: Digite seu apelido");
+                            Define_Aviso();
+                            seleção_ativa = false;
+                        } else if(apelido_jogador.length() == 0 && nome_jogador.length() == 0) {
+                            cerr << "Aviso: Digite seu nome e apelido";
+                            cout << endl;
+                            aviso.setString("Aviso: Digite seu nome e apelido");
                             Define_Aviso();
                             seleção_ativa = false;
                         } else {
                             cerr << "Aviso: Jogador já existente";
-                            aviso.setString("Aviso: Jogador já existente");
+                            cout << endl;
+                            aviso.setString("Aviso: Jogador ja existente");
                             Define_Aviso();
                             seleção_ativa = false;
                         }
